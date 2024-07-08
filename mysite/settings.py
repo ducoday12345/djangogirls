4525b73e2e8480506e69fe319d6f4d071666f0fe
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2024-07-08 11:32:18 -0400

    update host location

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-r88-ap*b(iox^i604ewq_d+spt7+_^7fs^=(0t73)^kxgu*&x1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
 # Application definition
 
@@ -118,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
